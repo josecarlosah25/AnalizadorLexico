5de1f63649022f75c3e461bcd1f9ce0036163e75
mandovg <mando_2211@hotmail.com>
2020-10-24 18:14:56 -0500

    Lista ligada 2.0, ya se podría implementar como tabla. También se agregó la función de búsqueda

--- listaLigada.c
+++ listaLigada.c
@@ -1,59 +1,83 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 int numClave = 0;
 
-typedef struct{
+typedef struct Nodo{
 	int clave;
 	char* palabra;
-	Nodo* siguiente;
+	struct Nodo* siguiente;
 }Nodo;
 
-typedef struct{
-	Nodo* head = NULL;
-	head->siguiente = NULL;
+typedef struct {
+	Nodo* head;
 
 }Lista;
 
+Lista crearLista(){
+	Lista lista;
+	lista.head = NULL;
+	return lista;
+}
+
 void insertar(Lista* listaActual, char* valor){
 	Nodo* temp = listaActual->head;
-	Nodo* actual = (Nodo *)malloc(sizeof(Node));
+	Nodo* actual = (Nodo *)malloc(sizeof(Nodo));
 
 	if(temp == NULL){
 		actual->clave = numClave;
 		actual->palabra = valor;
-		actual->siguiente = temp;
-		temp = actual;
-		temp->siguiente = NULL;
-
+		actual->siguiente = NULL;
+		listaActual->head = actual;
+		//printf("Si inserte\n");
 	}
+	else{
+		while(temp->siguiente != NULL)
+			temp = temp->siguiente;
+			//if(temp->siguiente == NULL){
+		numClave++;
+		actual->clave = numClave;
+		actual->palabra = valor;
+		actual->siguiente = NULL;
+		temp->siguiente = actual;
+		//printf("Si inserte\n");
+			
+	}
+	
 
-	while(temp != NULL){
-		if(temp->siguiente == NULL){
-			actual->clave = numClave + 1;
-			actual->palabra = valor;
-			actual->siguiente = temp;
-			temp->siguiente = actual;
+}
+
+void imprimirLista(Lista listaActual){
+	
+	if (listaActual.head == NULL)
+		printf("Lista vacia\n");
+	else{
+		Nodo* ptr = listaActual.head;
+		while(ptr!= NULL){
+			printf("%d -> %s\n", ptr->clave, ptr->palabra);
+			ptr = ptr->siguiente;
 		}
-		temp = temp->siguiente;
-
-	}
-
-}
-
-void imprimirLista(Lista* listaActual){
-	Nodo* 
-}
-
-
-Nodo* crearLista(int cantidadNodos, Lista listaActual){
-	Nodo* head = NULL;
-	Nodo* temp = NULL;
-
-	for(int i = 0; i < cantidadNodos; i++){
-		temp = (Nodo*)malloc(sizeof(Nodo));
-
 	}
 }
 
+void busqueda(Lista* listaActual, int dato){
+    Nodo *temp = listaActual->head;
+    while (temp != NULL) {
+        if (temp->clave == dato){
+            printf("\n Se encuentra el numero %d en la lista \n", dato);
+            break;
+        }
+        else
+            temp = temp->siguiente;
+    }
+}
+
+int main(){
+	Lista tabla = crearLista();
+	insertar(&tabla, "hola");
+	insertar(&tabla, "buenos");
+	insertar(&tabla, "dias");
+	insertar(&tabla, "a");
+	insertar(&tabla, "todos");
+	imprimirLista(tabla);
+}
